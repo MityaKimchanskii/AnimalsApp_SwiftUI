5e296d641aca4b18034538794d58a7d7d10edd13
Dmitrii Kim <mitya_kim@icloud.com>
2022-11-06 14:25:29 -0800

    feat: added animalList, animalItemView

--- AfricanAnimals_SwiftUI/App/ContentView.swift
+++ AfricanAnimals_SwiftUI/App/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - Properties
+    let animals: [Animal] = Bundle.main.decode("animals.json")
     
     // MARK: - Body
     var body: some View {
@@ -19,6 +20,10 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    AnimallistItemView(animal: animal)
+                }
             }//: List
             .navigationBarTitle("Animals", displayMode: .large)
         }//: Navigation
