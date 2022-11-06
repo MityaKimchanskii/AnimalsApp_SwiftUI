cdb932b8c66467e0fe2c7460597ba30031fb1354
Dmitrii Kim <mitya_kim@icloud.com>
2022-11-06 11:07:55 -0800

    Initial Commit

---
+++ AfricanAnimals_SwiftUI/AfricanAnimals_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricanAnimals_SwiftUIApp.swift
+//  AfricanAnimals_SwiftUI
+//
+//  Created by Mitya Kim on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricanAnimals_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
