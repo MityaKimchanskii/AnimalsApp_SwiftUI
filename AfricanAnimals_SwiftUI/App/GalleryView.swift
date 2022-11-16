9cc9b23ac86d99f096519a0d68b6b241d8de0af0
Dmitrii Kim <mitya_kim@icloud.com>
2022-11-15 18:34:54 -0800

    feat: added GalleryView, Grid, animation

--- AfricanAnimals_SwiftUI/App/GalleryView.swift
+++ AfricanAnimals_SwiftUI/App/GalleryView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - Properties
+    //let gridLayout: [GridItem] = [
+    //    GridItem(.flexible()),
+    //    GridItem(.flexible()),
+    //    GridItem(.flexible())
+    //]
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
+    // MARK: - Body
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn) { newValue in
+                        gridSwitch()
+                    }
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    }//:Loop
+                }//:Grid
+                .animation(.easeIn)
+                .onAppear {
+                    gridSwitch()
+                }
+            }//:VStack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
+        }//:Scroll
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
+// MARK: - Preview
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView()
